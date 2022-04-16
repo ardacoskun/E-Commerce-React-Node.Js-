66de58e763629789736ecb83638c54a6493043a6
Arda Coşkun <m.ardacoskun@gmail.com>
2022-04-16 20:18:27 +0300

    error middleware created

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ const app = express();
 
 const categoryRoutes = require("./routes/categoryRoutes");
 const notFoundMiddleware = require("./middleware/notFound");
+const errorHandlerMiddleware = require("./middleware/errorHandler");
 
 // app.get("/", (req, res) => {
 //   res.send("working");
@@ -15,6 +16,7 @@ const notFoundMiddleware = require("./middleware/notFound");
 app.use("/", categoryRoutes);
 
 app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
